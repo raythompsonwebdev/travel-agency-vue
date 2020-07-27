52c02443b2bab1fc1f3e45207e3deab98c796396
raythompsonwebdev <ray_thomp@hushmail.com>
2020-07-28 00:15:18 +0100

    retreived updated code for components

--- src/App.vue
+++ src/App.vue
@@ -1,130 +1,176 @@
 <template>
-	<div id="app">
-		<!--wrapper-->
-		<div id="wrapper">
-		<!--header-->
-		<header>
-			<!---contact number-->
-			<aside class="contact">
-			<span class="tele">
-				<img v-bind:src="'src/assets/images/Telephone.png'">
-				<a href="tel:0208-000-1452" id="contact-number">{{phoneNumber}}</a>
-			</span>
-			<span id="opening-time">{{openTime}}</span>
-			</aside>
+  <div id="app">
+    <!--wrapper-->
+    <div id="wrapper">
+      <!--header-->
+      <header>
+        <!---logo-->
+        <figure id="logo">
+          <img :src="'assets/images/travel-agency-website-logo.png'" />
+        </figure>
 
-			<!---logo-->
-			<figure id="logo">
-			<img v-bind:src="'./src/assets/images/travel-agency-website-logo.png'">
-			</figure>
+        <!--<div id="midsection"></div>-->
 
-			<!---primary navigation-->
-			<nav>
-			<aside id="language-box">
-				<form>
-				<label>Language:</label>
-				<select v-model="languages" name="language">
-					<option v-for="(language,index) in languages" :key="index">{{language.title}}</option>
-				</select>
-				</form>
-			</aside>
+        <!---contact number-->
+        <aside class="contact">
+          <span class="tele">
+            <img :src="'assets/images/Telephone.png'" />
+            <a :href="`tel:${phoneNumber}`" id="contact-number">{{phoneNumber}}</a>
+          </span>
+          <span id="opening-time">{{openTime}}</span>
+        </aside>
+        <nav>
+          <!--LanguageSelect Item component -->
+          <aside id="language-box">
+            <form>
+              <label for="languages">Language:</label>
+              <div id="flagImg">
+                <img @error="imgUrlAlt" :src="'/assets/images/flags/' + selected + '.jpg'" />
+              </div>
+              <select
+                name="languages"
+                v-model="selected"
+                v-on:click="itemsSearched($event.target.value);"
+              >
+                <option
+                  v-for="(language, i) in onChangeImg"
+                  :value="language.value"
+                  :key="i"
+                >{{language.name}}</option>
+              </select>
+            </form>
+          </aside>
 
-			<button id="toggle-menu">Menu</button>
+          <button v-on:click="naviFunction" id="toggle-menu">Menu</button>
 
-			<ul id="navi" class="hide">
-				<li>
-				<router-link to="/" class="nav-item r-item">Home</router-link>
-				</li>
-				<li>
-				<router-link to="/HolidayPackages" class="nav-item r-item">HolidayPackages</router-link>
-				</li>
-				<li>
-				<router-link to="/BestDeal" class="nav-item r-item">BestDeal</router-link>
-				</li>
-				<li>
-				<router-link to="/Services" class="nav-item r-item">Services</router-link>
-				</li>
-				<li>
-				<router-link to="/AboutUs" class="nav-item r-item">AboutUs</router-link>
-				</li>
-				<li>
-				<router-link to="/Contact" class="nav-item r-item">Contact</router-link>
-				</li>
-			</ul>
-			</nav>
-			<!---primary navigation end-->
-		</header>
+          <ul id="navi" class="hide" v-if="naviFunction">
+            <li>
+              <router-link to="/" class="nav-item r-item">Home</router-link>
+            </li>
+            <li>
+              <router-link to="/HolidayPackages" class="nav-item r-item">HolidayPackages</router-link>
+            </li>
+            <li>
+              <router-link to="/BestDeal" class="nav-item r-item">BestDeal</router-link>
+            </li>
+            <li>
+              <router-link to="/Services" class="nav-item r-item">Services</router-link>
+            </li>
+            <li>
+              <router-link to="/AboutUs" class="nav-item r-item">AboutUs</router-link>
+            </li>
+            <li>
+              <router-link to="/Contact" class="nav-item r-item">Contact</router-link>
+            </li>
+          </ul>
+        </nav>
+      </header>
 
-		<transition name="router-anim">
-			<!--view-->
-			<router-view/>
-		</transition>
-		</div>
+      <transition name="router-anim">
+        <!--view-->
+        <router-view></router-view>
+      </transition>
+    </div>
 
-		<!--footer-->
-		<footer>
-		<ul>
-			<li>
-			<router-link to="/" class="nav-item r-item">Home</router-link>
-			</li>
-			<li>
-			<router-link to="/BestDeal" class="nav-item r-item">BestDeal</router-link>
-			</li>
-			<li>
-			<router-link to="/AboutUs" class="nav-item r-item">AboutUs</router-link>
-			</li>
-			<li>
-			<router-link to="/HolidayPackages" class="nav-item r-item">HolidayPackages</router-link>
-			</li>
-			<li>
-			<router-link to="/Contact" class="nav-item r-item">Contact</router-link>
-			</li>
-			<li>
-			<router-link to="/Services" class="nav-item r-item">Services</router-link>
-			</li>
-		</ul>
-		</footer>
-	</div>
+    <!--footer-->
+    <footer>
+      <ul>
+        <li>
+          <router-link to="/" class="nav-item r-item">Home</router-link>
+        </li>
+        <li>
+          <router-link to="/BestDeal" class="nav-item r-item">BestDeal</router-link>
+        </li>
+        <li>
+          <router-link to="/AboutUs" class="nav-item r-item">AboutUs</router-link>
+        </li>
+        <li>
+          <router-link to="/HolidayPackages" class="nav-item r-item">HolidayPackages</router-link>
+        </li>
+        <li>
+          <router-link to="/Contact" class="nav-item r-item">Contact</router-link>
+        </li>
+        <li>
+          <router-link to="/Services" class="nav-item r-item">Services</router-link>
+        </li>
+      </ul>
+    </footer>
+  </div>
 </template>
 
 <script>
+import axios from "axios";
 export default {
   name: "app",
-  methods: {},
-  data() {
+  mounted() {
+    axios
+      .get("./data-json/languageselect.json")
+      .then(response => (this.languages = response.data));
+  },
+  methods: {
+    naviFunction: function(event) {
+      event.preventDefault();
+      var el = document.querySelector("#navi");
+      var box = el.getAttribute("class");
+      if (box == "hide") {
+        el.setAttribute("class", "show");
+      } else {
+        el.setAttribute("class", "hide");
+      }
+    },
+    itemsSearched: function(id) {
+      this.selected = id;
+    },
+    imgUrlAlt(event) {
+      event.target.src = "/assets/images/flags/default.jpg";
+    }
+  },
+  data: () => {
     return {
       phoneNumber: "0208-365-1452",
       openTime: "Everyday 8am - 9pm",
-
-      languages: [
-        { title: "English" },
-        { title: "Chinese" },
-        { title: "German" },
-        { title: "French" },
-        { title: "Spanish" },
-        { title: "Arabic" }
-      ]
+      selected: " ",
+      defaultImage: "/assets/images/flags/default.jpg",
+      languages: []
     };
+  },
+  computed: {
+    onChangeImg: function() {
+      if (this.selected === "nothing") {
+        return this.languages;
+      } else if (this.selected === "United_Kingdom") {
+        return this.languages;
+      } else if (this.selected === "Germany") {
+        return this.languages;
+      } else if (this.selected === "United_States") {
+        return this.languages;
+      } else if (this.selected === "Jamaica") {
+        return this.languages;
+      } else if (this.selected === "France") {
+        return this.languages;
+      } else if (this.selected === "Spain") {
+        return this.languages;
+      } else {
+        return this.languages;
+      }
+    }
   }
 };
 </script>
 
 <style lang="scss">
 @import "main.scss";
-
 .router-anim-enter-active {
-  animation: coming 1s;
-  animation-delay: 1s;
+  animation: coming 0.5s;
+  animation-delay: 0.5s;
   opacity: 0;
 }
-
 .router-anim-leave-active {
-  animation: going 1s;
+  animation: going 0.5s;
 }
-
 @keyframes coming {
   from {
-    transform: translateX(-50px);
+    transform: translateX(-200px);
     opacity: 0;
   }
   to {
@@ -132,13 +178,12 @@ export default {
     opacity: 1;
   }
 }
-
 @keyframes going {
   from {
     transform: translateX(0);
   }
   to {
-    transform: translateX(-50px);
+    transform: translateX(-200px);
     opacity: 0;
   }
 }
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,384 +1,69 @@
 <template>
-	<div class="Home page">
+  <div class="Home page">
+    <button v-on:click="leftSideFunction" id="side-bar-btn">SIDE</button>
+    <!--Leftside-->
+    <aside id="leftside" class="hide" v-if="leftSideFunction">
+      <!---Search form-->
+      <SearchForm />
+      <!--Destinations-->
+      <Destinations :destinationitems="this.data.destinationitems" />
 
-		<button id="side-bar-btn" v-on:click="leftSideFunction">SIDE</button>
-		<!--Leftside-->
-		<aside id="leftside" class="hide" v-if="leftSideFunction">
+      <br />
+      <br />
+      <br />
+    </aside>
 
-			<!---Search form-->
-			<section class="leftsidebox">
+    <main id="content">
+      <!--Banner Image-->
+      <figure id="banner">
+        <img :src="'assets/images/travel-agency-website-banner-image.jpg'" :alt="'banner-image'" />
+      </figure>
 
-			<h2>Holiday Search</h2>
+      <!--Featured Holiday Packages-->
+      <FeaturedHolidays :featuredholidayitems="this.data.featuredholidayitems" />
+    </main>
 
-			<form id="search_form" >
-
-				<ul>
-				<li>
-					<label for="searchdestination">Where are you going?</label>
-
-					<select v-model="searchDestination" name="searchdestination" class="destination1">
-					
-					<option v-for="(searchDestinations,index) in searchDestination" :key='index' >{{searchDestinations}}</option>
-
-							</select>
-					
-				</li>
-
-				<li class="time">
-					<label>When are you going?</label>
-
-					<select v-model="searchday" name="date" class="time_1">
-					
-					<option v-for="(searchday,index) in searchday" :key='index'>{{searchday}}</option>
-					</select>
-
-					<select v-model="searchmonth" name="month" id="time_2">
-				
-					<option v-for="(searchmonth,index) in searchmonth" :key='index'>{{searchmonth}}</option>
-					</select>
-
-					<select v-model="searchyear" name="year" class="time_1">
-					<option v-for="(searchyear,index) in searchyear" :key='index'>{{searchyear}}</option>
-					
-					</select>
-
-				</li>
-
-				<li>
-					<label> Duration</label>
-					<select v-model="searchduration" name="duration" class="destination1">
-					<option disabled value="">Please select one</option>
-					<option v-for="(searchduration,index) in searchduration" :key='index'>{{searchduration}}</option>
-							</select>
-				</li>
-
-				<li class="misc_inner">
-					<label> Board</label>
-					<select v-model="searchboard" name="board" class="misc">
-					<option v-for="(searchboard,index) in searchboard" :key='index'>{{searchboard}}</option>
-					</select>
-				</li>
-
-				<li class="misc_inner">
-					<label> Stars</label>
-					<select v-model="searchstar" name="stars" class="misc">
-					<option v-for="(searchstar,index) in searchstar" :key='index'>{{searchstar}}</option>
-							</select>
-				</li>
-
-				<li class="misc_inner">
-					<label>Adults</label>
-					<select v-model="searchadult" name="adults" class="misc">
-					<option v-for="(searchadult,index) in searchadult" :key='index'>{{searchadult}}</option>
-							</select>
-				</li>
-				<li class="misc_inner">
-					<label>Children 0-17</label>
-					<select v-model="searchchildren" name="children" class="misc">
-					<option v-for="(searchchildren,index) in searchchildren" :key='index'>{{searchchildren}}</option>
-					</select>
-				</li>
-				</ul>
-
-				<input type="submit" value="Find Holiday" id="searchbtn">
-
-			</form>
-
-			</section>
-
-			<!--Destinations-->
-			<section class="leftsidebox">
-
-			<h2>Top Destinations</h2>
-
-			<div class="destinations" v-for="(holidayspot,index) in holidayspots" :key='index' >
-				<figure>
-				<img v-bind:src="holidayspot.url">
-
-				<figcaption>
-					<h3>{{holidayspot.title}}</h3>
-					<p>{{holidayspot.country}}</p>
-					<p>{{holidayspot.hotels}}</p>
-				</figcaption>
-
-				</figure>
-			</div>
-			
-			<a href="">View All</a>
-
-			</section>
-
-			<br/><br/><br/>
-
-		</aside>
-
-		<main id="content">
-
-			<figure id="banner">
-				<img src='src/assets/images/travel-agency-website-banner-image.jpg'>
-			</figure>
-
-			<section id="offers">
-
-				<h2>Featured Holiday Package</h2>
-
-				<div class="offerbox">
-					<figure>
-					<img src='src/assets/images/travel-agency-website-spanish-villa-image.jpg'>
-
-					<figcaption>
-						<p>Lorem Ipsum Dolen sit.</p>
-						<p>from only <span class="offerbox-price">&pound;339</span> per person</p>
-					</figcaption>
-
-					</figure>
-				</div>
-				<div class="offerbox">
-					<figure>
-					<img src='src/assets/images/travel-agency-website-taj-mahal-image.jpg'>
-					<figcaption>
-						<p>Lorem Ipsum Dolen sit.</p>
-						<p>from only <span class="offerbox-price">&pound;339</span> per person</p>
-					</figcaption>
-					</figure>
-				</div>
-				<div class="offerbox">
-					<figure>
-					<img src='src/assets/images/travel-agency-website-gold-coast-image.jpg'>
-					<figcaption>
-						<p>Lorem Ipsum Dolen sit.</p>
-						<p>from only <span class="offerbox-price">&pound;339</span> per person</p>
-					</figcaption>
-					</figure>
-
-					<div class="clearfix"></div>
-				</div>
-				<div class="offerbox">
-					<figure>
-					<img src='src/assets/images/travel-agency-website-dubai-image.jpg'>
-
-					<figcaption>
-						<p>Lorem Ipsum Dolen sit.</p>
-						<p>from only <span class="offerbox-price">&pound;339</span> per person</p>
-					</figcaption>
-					</figure>
-				</div>
-				<div class="offerbox">
-					<figure>
-					<img src='src/assets/images/travel-agency-website-singapore-image.jpg'>
-					<figcaption>
-						<p>Lorem Ipsum Dolen sit.</p>
-						<p>from only <span class="offerbox-price">&pound;339</span> per person</p>
-					</figcaption>
-					</figure>
-				</div>
-				<div class="offerbox">
-					<figure>
-					<img src='src/assets/images/travel-agency-website-toronto-image.jpg'>
-
-
-					<figcaption>
-						<p>Lorem Ipsum Dolen sit.</p>
-						<p>from only <span class="offerbox-price">&pound;339</span> per person</p>
-					</figcaption>
-					</figure>
-				</div>
-
-				<div class="clearfix"></div>
-
-			</section>
-
-		</main>
-
-		<div class="clearfix"></div>
-	</div>
+    <div class="clearfix"></div>
+  </div>
 </template>
 
 <!--scripting-->
 <script>
-	export default {
-
-		methods: {
-
-			leftSideFunction: function() {
-
-			console.log(event);
-
-			event.preventDefault();
-
-			var el = document.getElementById("leftside");
-
-			var box = el.getAttribute("class");
-
-			if (box == "hide") {
-
-				el.setAttribute("class", "show");
-
-			} else {
-				el.setAttribute("class", "hide");
-
-			}
-			}
-
-		},
-
-		name: 'Home',
-		
-
-		data() {
-
-			return {
-
-			show: 'true',
-									
-			searchDestination: [
-				'Istanbul', 
-				'HongKong', 
-				'Paris', 
-				'Bangkok', 
-				'London', 
-				'Dubai', 
-				'Sydney'
-			],
-
-			searchday: [
-				1,
-				2, 
-				3,
-				4, 
-				5, 
-				6,  
-				7, 
-				8,
-				9, 
-				10, 
-				11, 
-				12, 
-				13, 
-				14,
-				15, 
-				16, 
-				17, 
-				18, 
-				19, 
-				20,
-				21,  
-				22,  
-				23,  
-				24, 
-				25,  
-				26,  
-				27, 
-				28, 
-				29,  
-				30,  
-				31,
-			],
-
-			searchmonth:[
-				'January',
-				'February',
-				'March',
-				'April',
-				'May',
-				'June', 
-				'July',
-				'August',
-				'September',
-				'October', 
-				'November',
-				'December'
-			],
-
-			searchyear:[
-				2018, 
-				2019, 
-				2020, 
-				2021, 
-				2022
-			],
-
-			searchduration:[
-				'Day Trip', 
-				'Weekend',
-				'One Week',
-				'Two Weeks',
-				'Three Weeks',
-				'One Month', 
-				'Three Months',
-				'Six Months'
-			],
-
-			searchboard:[
-				'Half',
-				'Full'
-			],
-
-			searchstar:[
-				'1 Star',
-				'2 Star',
-				'3 Star',
-				'4 Star', 
-				'5 Star'
-			],
-
-			searchadult:[
-				'1',
-				'2', 
-				'3',
-				'4', 
-				'5', 
-				'6'
-			],
-
-			searchchildren:[
-				'1',
-				'2', 
-				'3',
-				'4', 
-				'5', 
-				'6'
-			],
-
-			holidayspots:[
-					
-				{
-					url :"src/assets/images/travel-agency-website-eiffel-tower-image.jpg",
-					title : 'Paris',
-					country : 'France',
-					hotels : '1,399 Hotels'
-				},
-				{
-					url :"src/assets/images/travel-agency-website-tower-of-london-image.jpg",
-					title : 'London',
-					country : 'England',
-					hotels : '1,912 Hotels'
-				},
-				{
-					url :"src/assets/images/travel-agency-website-bangkok-image.jpg",
-					title : 'Bangkok',
-					country : 'Thailand',
-					hotels : '1,542 Hotels'
-				},
-				{
-					url :"src/assets/images/travel-agency-website-istanbul-image.jpg",
-					title : 'Istanbul',
-					country : 'Turkey',
-					hotels : '504 Hotels'
-				},
-				{
-					url :"src/assets/images/travel-agency-website-hong-kong-image.jpg",
-					title : 'Hong Kong',
-					country : 'China',
-					hotels : '504 Hotels'
-				},
-
-			]
-
-			}
-
-		}
-	}
+import SearchForm from "./SearchForm.vue";
+import Destinations from "./Destinations.vue";
+import FeaturedHolidays from "./FeaturedHolidays.vue";
+import axios from "axios";
+export default {
+  name: "Home",
+  methods: {
+    leftSideFunction: function(event) {
+      event.preventDefault();
+      var el = document.getElementById("leftside");
+      var box = el.getAttribute("class");
+      if (box == "hide") {
+        el.setAttribute("class", "show");
+      } else {
+        el.setAttribute("class", "hide");
+      }
+    }
+  },
+  components: {
+    Destinations,
+    FeaturedHolidays,
+    SearchForm
+  },
+  mounted() {
+    axios
+      .get("./data-json/data-json.json")
+      .then(response => (this.data = response.data));
+  },
+  data() {
+    return {
+      data: [],
+      show: true
+    };
+  }
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
