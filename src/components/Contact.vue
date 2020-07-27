d84594ff5ca141303d10d35692dca9f2de77ebd8
raythompsonwebdev <ray_thomp@hushmail.com>
2020-07-27 22:16:27 +0100

    out of blue merge

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,5 +1,4 @@
 <template>
-<<<<<<< HEAD
 <div class="Contact">
 	<button v-on:click="contactFunction" id="side-bar-btn">CONTACT</button>
 
@@ -55,55 +54,6 @@
 		<div class="clearfix"></div>
 	</main>
 	<div class="clearfix"></div>
-=======
-  <div class="Contact">
-    <button v-on:click="contactFunction" id="side-bar-btn">Contact</button>
-
-    <!--contact details box-->
-    <aside id="contactside" class="hide" v-if="contactFunction">
-      <ul id="contact-details">
-        <li>
-          <span>
-            <img v-bind:src="'assets/images/Telephone.png'" />
-          </span>
-          Phone:
-          <a href="tel:123 436 7890">123 436 7890</a>
-        </li>
-        <li>
-          <span>
-            <img v-bind:src="'assets/images/envelope.png'" />
-          </span>
-          Mail:
-          <a href="mailto:info@travelagency.com">info@travelagency.com</a>
-        </li>
-        <li>
-          <span>
-            <img v-bind:src="'assets/images/Address.png'" />
-          </span>
-          Address:
-          <br />Lorem Ipsum 1234 Dolo Sit Amet
-        </li>
-      </ul>
-      <div class="clearfix"></div>
-    </aside>
-
-    <main id="content2">
-      <h1>Lorem ipsum dolor sit amet, consectetur</h1>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-        <br />Enim odit dolor sequi voluptatem esse doloribus libero commodi ea debitis id nostrum odio velit impedit vitae tenetur voluptates vero beatae excepturi. Lorem ipsum dolor sit amet, consectetur
-        adipisicing elit.
-        <br />Enim odit dolor sequi voluptatem esse doloribus libero commodi ea debitis id nostrum odio velit impedit vitae tenetur voluptates vero beatae excepturi.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-        <br />Enim odit dolor sequi voluptatem esse doloribus libero commodi ea debitis id nostrum odio velit impedit vitae tenetur voluptates vero beatae excepturi.
-      </p>
-      <div class="clearfix"></div>
-      <br />
-
-      <ContactForm @review-submitted="addReview"></ContactForm>
->>>>>>> 1b2f91303d694f93f1f4b8687c7692c699f7c16d
 
       <div class="clearfix"></div>
     </main>
