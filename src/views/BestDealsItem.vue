3402e0674207c7b684509c3ed59f774de9afa0ad
raythompsonwebdev <ray_thomp@hushmail.com>
2021-03-06 22:49:00 +0000

    created json files

--- src/views/BestDealsItem.vue
+++ src/views/BestDealsItem.vue
@@ -21,8 +21,18 @@
 
         <figcaption>
           <h3>Location: {{ bestdealitem.location }}.</h3>
-          
-          <button>Book Holiday</button>
+
+          <!-- add @click="addToCart" to button -->
+          <!-- add v-if="!showSuccessmessage" to button if false -->          
+          <button>
+            Book Holiday
+          </button>          
+          <!-- v-if="showSuccessmessage" button if true -->
+          <!-- make button different color -->
+          <!-- no click event -->
+          <!-- <button>
+            Book Holiday Success
+          </button> -->
         </figcaption>
       </figure>
     </article>
@@ -33,7 +43,6 @@
 
 <script>
 
-//import { bestdealitems } from '../data-json.js';
 import NotFoundpage from '../views/NotFoundpage';
 import axios from "axios";
 
@@ -45,13 +54,28 @@ export default {
   data(){
     return{
       bestdealitem: {},
+      showSuccessMessage: false,
     }
-  },
+  },  
   async created(){
     const result = await axios.get(`/api/bestdeals/${this.$route.params.id}`);
     const bestdealitem = result.data;
     this.bestdealitem = bestdealitem;
-  }
+  },
+  // methods:{
+  //   async addToCart(){
+  //     //add request body object as second argument
+  //     await axios.post('/api/users/1234/cart/', {
+  //       bestdealId: this.$route.params.id,
+  //     });
+          // this.showSuccessMessage = true;
+
+          // setTimeout(()=>{
+          //   this.$router.push('/bestdeals');
+          // }, 1500);
+          
+  //   }
+  // },
     
 };
 </script>
--- src/views/Cart.vue
+++ src/views/Cart.vue
@@ -9,23 +9,32 @@
 </template>
 
 <script>
+import axios from "axios";
 export default {
   name: "Cart",
   data(){
     return{
       title: "Shopping Cart page",
+      cartItems: {},
       
     }
   },
-  // computed:{
-  //   //adds cart item prices
-  //   totalPrice(){
-  //     return this.cartItems.reduce(
-  //       (sum, item) => sum + Number(item,price),
-  //       0,
-  //     );
-  //   }
-  // }
+  computed:{
+    //adds cart item prices
+    totalPrice(){
+      return this.cartItems.reduce(
+        (sum, item) => sum + Number(item.price),
+        0,
+      );
+    }
+  },
+  async created () {
+    const result = await axios.get('/api/users/12335/cart');
+    const cartItems = result.data;
+    this.cartItems = cartItems;
+
+
+  },
   
 };
 </script>
