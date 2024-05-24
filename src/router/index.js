f4e8f4ebb78e6e9c9df9e6c8c1938b4346b39cb2
raythompsonwebdev <raymond.thompson@raythompsonwebdev.co.uk>
2024-05-24 23:38:45 +0100

    updated home page search form

--- src/router/index.js
+++ src/router/index.js
@@ -78,8 +78,6 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
-  linkActiveClass: "active",
-  linkExactActiveClass: "exact-active",
 });
 
 export default router;
